54622f780f41008638340b347671f8b87d2cbffe
Julio Sanchez <jsanchez191980802@gmail.com>
2018-10-07 18:02:18 -0500

    firebase Auth is mostly working - added logout button

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -9,3 +9,58 @@
     messagingSenderId: "589336015645"
   };
   firebase.initializeApp(config);
+
+  var emailTxt = $('#emailInput');
+  var passTxt = $('#passwordInput');
+  var subBtn = $('#realSignUpButton');
+  var logIn = $('#logInButton');
+  var logOut = $('#logOutButton');
+
+
+  //log in event
+  logIn.on('click', function(e){
+    e.preventDefault();
+    //get email and password text
+    var email = emailTxt.val().trim();
+    var password = passTxt.val().trim();
+    var auth = firebase.auth();
+    //sign in
+    var promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+
+    //this should send user back to homepage after login
+    window.location.href="index.html";
+  })
+
+  //add signup event
+  subBtn.on('click', function(e){
+    e.preventDefault();
+    //get email and password text
+    var email = emailTxt.val().trim();
+    var password = passTxt.val().trim();
+    var auth = firebase.auth();
+    //sign in
+    var promise = auth.createUserWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+
+    //clear input fields
+    var email = emailTxt.text('');
+    var password = passTxt.text('');
+  });
+
+  //Logout the user
+  logOut.on('click', function(){
+      firebase.auth().signOut();
+  });
+
+
+  //add a realtime listener
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser) {
+          console.log(firebaseUser);
+          logOut.removeClass('invisible');
+
+      }else{
+          console.log('not logged in');
+      }
+  });
