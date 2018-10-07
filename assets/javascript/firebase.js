e9152221af03840dc76ccdc48ee22fb4fb856e1d
Julio Sanchez <jsanchez191980802@gmail.com>
2018-10-07 15:29:52 -0500

    log in page, firebase.js, added slide to YT content

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAmqBOMa3ihtUD1j1V0SEKpdaVx_N_y2l0",
+    authDomain: "bored-hub.firebaseapp.com",
+    databaseURL: "https://bored-hub.firebaseio.com",
+    projectId: "bored-hub",
+    storageBucket: "bored-hub.appspot.com",
+    messagingSenderId: "589336015645"
+  };
+  firebase.initializeApp(config);
