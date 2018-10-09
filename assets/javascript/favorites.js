bbc5a898e9e186972c5573e468396fd0f1c3f653
Alexander Osborn <misterzander@gmail.com>
2018-10-09 12:40:25 -0500

    A little off the top

--- assets/javascript/favorites.js
+++ assets/javascript/favorites.js
@@ -1,17 +1,8 @@
-// var config = {
-//     apiKey: "AIzaSyAmqBOMa3ihtUD1j1V0SEKpdaVx_N_y2l0",
-//     authDomain: "bored-hub.firebaseapp.com",
-//     databaseURL: "https://bored-hub.firebaseio.com",
-//     projectId: "bored-hub",
-//     storageBucket: "bored-hub.appspot.com",
-//     messagingSenderId: "589336015645"
-//   };
-//   firebase.initializeApp(config);
   var db = firebase.database();
   var email = "myfancyemail@gmailDOTcom";
 
   db.ref().on("value", function(snapshot) {
-    $("#favs").empty(); 
+    //$("#favs").empty(); 
     var favorites = snapshot.child("users/" + email + "/favorites").val();
      console.log(favorites);
     for(var sha in favorites) {
